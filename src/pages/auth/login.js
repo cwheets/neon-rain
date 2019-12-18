a3e6265c7ef6e1dd90cc2a6578231882840d30c8
Stimoe <stimoe3@gmail.com>
2019-12-17 17:01:11 -0800

    trying to connect the servers

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,103 +1,103 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import Axios from "axios";
-import "./style.css";
+// import React, { Component } from "react";
+// import { Link } from "react-router-dom";
+// import Axios from "axios";
+// import "./style.css";
 
 
-class Login extends Component {
-  constructor() {
-    super();
-    this.state = {
-      username: "",
-      password: "",
-      errors: {}
-    };
-  }
-  onChange = e => {
-    this.setState({ [e.target.id]: e.target.value });
-  };
-  onSubmit = e => {
-    e.preventDefault();
-    const userData = {
-      username: this.state.username,
-      password: this.state.password
-    };
+// class Login extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       username: "",
+//       password: "",
+//       errors: {}
+//     };
+//   }
+//   onChange = e => {
+//     this.setState({ [e.target.id]: e.target.value });
+//   };
+//   onSubmit = e => {
+//     e.preventDefault();
+//     const userData = {
+//       username: this.state.username,
+//       password: this.state.password
+//     };
     
 
-    Axios.post("https://express-neon-rain-game.herokuapp.com/api/users/login", userData)
-      .then(data => {
-        console.log(data);
-        this.props.history.push("/battlepage");
-      })
-      .catch(err => {
-        console.log(err.response);
-        alert("Username doesnt exists or password was wrong")
-      });
+//     Axios.post("https://express-neon-rain-game.herokuapp.com/api/users/login", userData)
+//       .then(data => {
+//         console.log(data);
+//         this.props.history.push("/battlepage");
+//       })
+//       .catch(err => {
+//         console.log(err.response);
+//         alert("Username doesnt exists or password was wrong")
+//       });
 
-    console.log(userData);
-  };
+//     console.log(userData);
+//   };
 
   
   
   
-  render() {
-    const { errors } = this.state;
-    return (
+//   render() {
+//     const { errors } = this.state;
+//     return (
       
-      <div className="container inputS">
-        <div className="landing3">
-          <div className="home-wrap3">
-            <div className="home-inner3"></div>
-          </div>
-        </div>
-        <div className="nes-container is-rounded is-dark">
-        <div style={{ marginTop: "4rem" }} className="row">
-          <div className="col s8 offset-s2">
-            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-              <h4>
-                <b>Login</b> below
-              </h4>
-              <p className="grey-text text-darken-1">
-                Don't have an account? <Link to="/register">Register</Link>
-              </p>
-            </div>
-            <form noValidate onSubmit={this.onSubmit}>
-              <div className="nes-field is-inline col s12">
-                <label htmlFor="username">Username</label>
-                <input
-                  error={errors.username}
-                  value={this.state.username}
-                  onChange={this.onChange} 
-                  type="username"
-                  id="username"
-                  className="nes-input nes-pointer neon1"
-                />
-              </div>
-              <div className="nes-field is-inline col s12">
-                <label htmlFor="password">Password</label>
-                <input
-                  className="nes-input neon1 nes-pointer"
-                  onChange={this.onChange}
-                  value={this.state.password}
-                  error={errors.password}
-                  id="password"
-                  type="password"
-                />
-              </div>
-              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                <button
-                  type="submit"
-                  className="loginBtn nes-pointer neon1 mb-3 nes-btn"
-                >
-                  Login
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-        </div>
-      </div>
-    );
-  }
-}
-export default Login;
+//       <div className="container inputS">
+//         <div className="landing3">
+//           <div className="home-wrap3">
+//             <div className="home-inner3"></div>
+//           </div>
+//         </div>
+//         <div className="nes-container is-rounded is-dark">
+//         <div style={{ marginTop: "4rem" }} className="row">
+//           <div className="col s8 offset-s2">
+//             <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+//               <h4>
+//                 <b>Login</b> below
+//               </h4>
+//               <p className="grey-text text-darken-1">
+//                 Don't have an account? <Link to="/register">Register</Link>
+//               </p>
+//             </div>
+//             <form noValidate onSubmit={this.onSubmit}>
+//               <div className="nes-field is-inline col s12">
+//                 <label htmlFor="username">Username</label>
+//                 <input
+//                   error={errors.username}
+//                   value={this.state.username}
+//                   onChange={this.onChange} 
+//                   type="username"
+//                   id="username"
+//                   className="nes-input nes-pointer neon1"
+//                 />
+//               </div>
+//               <div className="nes-field is-inline col s12">
+//                 <label htmlFor="password">Password</label>
+//                 <input
+//                   className="nes-input neon1 nes-pointer"
+//                   onChange={this.onChange}
+//                   value={this.state.password}
+//                   error={errors.password}
+//                   id="password"
+//                   type="password"
+//                 />
+//               </div>
+//               <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+//                 <button
+//                   type="submit"
+//                   className="loginBtn nes-pointer neon1 mb-3 nes-btn"
+//                 >
+//                   Login
+//                 </button>
+//               </div>
+//             </form>
+//           </div>
+//         </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+// export default Login;
--- src/pages/auth/register2.js
+++ src/pages/auth/register2.js
@@ -1,74 +1,74 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import Axios from "axios";
+// import React, { Component } from "react";
+// import { Link } from "react-router-dom";
+// import Axios from "axios";
 
-import SignUpFormNew from "../../components/registerForm/RegisterForm"
-import Secret from "../../components/secrets/Secret"
-class RegisterNew extends Component {
+// import SignUpFormNew from "../../components/registerForm/RegisterForm"
+// import Secret from "../../components/secrets/Secret"
+// class RegisterNew extends Component {
 
-    state = {
-        username:"",
-        password:"",
-        loggedInUser:"",
-        errors: {},
-        url:"http://localhost:8080",
-        // url:"https://manateepi.herokuapp.com",
+//     state = {
+//         username:"",
+//         password:"",
+//         loggedInUser:"",
+//         errors: {},
+//         url:"http://localhost:8080",
+//         // url:"https://manateepi.herokuapp.com",
        
-      }
+//       }
   
 
-    handleSignupFormSubmit = event=>{
-      event.preventDefault();
-      Axios.post(`${this.state.url}/auth/signup`,{username:this.state.username,password:this.state.password},{withCredentials:true}).then(res=>{
-        console.log(res.data,res.status)
-        this.handleLoginFormSubmit();
-      }).catch(err=>{
-        console.log(err.response);
-      })
-    }
+//     handleSignupFormSubmit = event=>{
+//       event.preventDefault();
+//       Axios.post(`${this.state.url}/auth/signup`,{username:this.state.username,password:this.state.password},{withCredentials:true}).then(res=>{
+//         console.log(res.data,res.status)
+//         this.handleLoginFormSubmit();
+//       }).catch(err=>{
+//         console.log(err.response);
+//       })
+//     }
 
-    handleChange= event=>{
-      const {username,value}=event.target;
-      this.setState({
-        [username]:value
-      })
-    }
-    onChange = e => {
-      this.setState({ [e.target.id]: e.target.value });
-    };
-  //   onSubmit = e => {
-  //     e.preventDefault();
-  // const newUser = {
-  //       username: this.state.username,
-  //       password: this.state.password,
-  //     };
-  // console.log(newUser);
-  // Axios.post("https://express-neon-rain-game.herokuapp.com/api/users/register", newUser).then(data => {
-  //   console.log(data);
-  //   this.props.history.push("/storypage")
-  // }).catch (err=> {
-  //   console.log(err);
-  //   alert("Username already exists or password could not be validated")
+//     handleChange= event=>{
+//       const {username,value}=event.target;
+//       this.setState({
+//         [username]:value
+//       })
+//     }
+//     onChange = e => {
+//       this.setState({ [e.target.id]: e.target.value });
+//     };
+//   //   onSubmit = e => {
+//   //     e.preventDefault();
+//   // const newUser = {
+//   //       username: this.state.username,
+//   //       password: this.state.password,
+//   //     };
+//   // console.log(newUser);
+//   // Axios.post("https://express-neon-rain-game.herokuapp.com/api/users/register", newUser).then(data => {
+//   //   console.log(data);
+//   //   this.props.history.push("/storypage")
+//   // }).catch (err=> {
+//   //   console.log(err);
+//   //   alert("Username already exists or password could not be validated")
     
-  // })
+//   // })
 
   
 
-    render() {
-        const { errors } = this.state;
-        return (
-            <div>
+//     render() {
+//         const { errors } = this.state;
+//         return (
+//             <div>
          
-         <SignUpFormNew username={this.state.username} password={this.state.password}handleChange={this.handleChange} handleSignupFormSubmit={this.handleSignupFormSubmit}/>
-        {this.state.loggedInUser?(
-          <div>
-            <Secret username={this.state.loggedInUser.username}/>
+//          <SignUpFormNew username={this.state.username} password={this.state.password}handleChange={this.handleChange} handleSignupFormSubmit={this.handleSignupFormSubmit}/>
+//         {this.state.loggedInUser?(
+//           <div>
+//             <Secret username={this.state.loggedInUser.username}/>
        
-          </div>
-          ):(null)}
+//           </div>
+//           ):(null)}
           
-           </div>
-    );
-    }
-}
-export default RegisterNew;
+//            </div>
+//     );
+//     }
+// }
+// export default RegisterNew;
